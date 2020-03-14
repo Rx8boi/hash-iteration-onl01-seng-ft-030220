3a74265ea6d4754eabfd3a628f0e690fc3c04375
Charles Butler <cdbutle@outlook.com>
2020-03-14 23:46:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ winner
 end
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kid, age| 
-    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  birthday_kids.each do |kids_name, age| 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
 end
